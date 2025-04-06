b371c5094ba7dca417a10933e93dcf79b4a85e70
Vihaan2312 <vihaanchoudhary2312@gmail.com>
2025-04-06 14:27:09 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,17 +3,4 @@ from google.cloud import firestore
 import pandas as pd
 import datetime as dt
 
-class OilApp:
-    def __init__(self):
-        # Initialization code
-        self.db = firestore.Client()
-        self.today = dt.datetime.now()
-        st.write("App initialized")
-
-    def run(self):
-        # Main logic of the app
-        st.write("Program is running")
-
-# Create an instance and run the app
-app = OilApp()
-app.run()
+st.write("Program is running")
