8501320073c243fa3c611552f50774d9a362dcd3
Vihaan2312 <vihaanchoudhary2312@gmail.com>
2025-04-07 09:05:29 +0530

    Update Create Order.py

--- pages/Create Order.py
+++ pages/Create Order.py
@@ -2,11 +2,8 @@ import streamlit as st
 from google.cloud import firestore
 import pandas as pd
 import datetime as dt
-import json
 
-# Load service account from secrets
-key_dict = st.secrets["firebase_service_account"]
-db = firestore.Client.from_service_account_info(key_dict)
+db = firestore.Client.from_service_account_info("Firestore.json")
 
 # Fetch existing customers from Firestore
 profiles_ref = db.collection("Profiles").stream()
