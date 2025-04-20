1cf00e8d938552dfbd220c0362b0eed915d14edc
Vihaan2312 <vihaanchoudhary2312@gmail.com>
2025-04-20 14:21:00 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,6 +3,6 @@ from google.cloud import firestore
 import pandas as pd
 import datetime as dt
 
-db = firestore.Client.from_service_account_info("Firestore.json")
+db = firestore.Client.from_service_account_json("Firestore.json")
 
 st.write("Program is running")
